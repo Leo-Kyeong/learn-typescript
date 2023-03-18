f858d3f42ebab3b4e6443cd5c725e3c2679b4706
Leo <drd9811@naver.com>
2023-03-18 18:39:42 +0900

    변수와 함수 타입 정의하기

---
+++ class-note/1_type_basic.ts
@@ -0,0 +1,31 @@
+// JS 문자열 선언
+// const str = 'hello';
+
+// TS 문자열 선언
+const str: string = 'hello';
+
+// TS 숫자
+const num: number = 10;
+
+// TS 배열
+const arr: Array<number> = [1,2,3];
+const heroes: Array<string> = ['Capt', 'Thor', 'Hulk'];
+const items: number[] = [1,2,3];
+
+// TS 튜플
+const address: [string, number] = ['gangNam', 100];
+
+// TS 객체
+const obj: object = {};
+// const person: object = {
+//   name: 'capt',
+//   age: 100
+// };
+
+const person: { name: string, age: number } = {
+    name: 'thor',
+    age: 1000
+};
+
+// TS 진위값
+const show: boolean = true;
